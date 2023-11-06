5af3cfe957a56c2fefdc05f5ab0ce391c5802844
Arutselvanrajkumar <145752344+Arutselvanrajkumar@users.noreply.github.com>
2023-11-07 00:15:01 +0530

    Add files via upload

---
+++ first.python.py
@@ -0,0 +1,10 @@
+# Get user input
+num = int(input("Enter a number: "))
+
+# Check if the number is positive, negative, or zero
+if num > 0:
+    print("The number is positive.")
+elif num < 0:
+    print("The number is negative.")
+else:
+    print("The number is zero.")
